28e242371b0f3a64646fd69cc228ce4037f733c7
gccunanan <gccunanan@gmail.com>
2019-12-11 20:13:20 -0800

    setup Provider and Store in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './components/App'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App';
+import reducers from './reducers';
 
 // This allows us to render our app in the public/index.html file
 // inside of the <div id="root"></div>
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(<Provider store={createStore(reducers)}><App /></Provider>, document.getElementById('root'));
