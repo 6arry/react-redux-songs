37d84f3cbd5878dcbcb2fb9a4e06dbd92c9dc34a
gccunanan <gccunanan@gmail.com>
2019-12-11 19:47:26 -0800

    in the src folder i created folders /actions /reducers and added index.js to actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
+const App = () => {
+    return (
+        <div>
+            App!
+        </div>
+    )
 }
+
+export default App;
