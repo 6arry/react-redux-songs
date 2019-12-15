2ff8b8d6e49481009adce65de4e440b6a704b49d
gccunanan <gccunanan@gmail.com>
2019-12-15 14:50:49 -0800

    setup redux into app; added SongDetail component; setup provider, connect, & mapStateToProps

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
 import App from './components/App';
 import reducers from './reducers';
 
 // This allows us to render our app in the public/index.html file
 // inside of the <div id="root"></div>
 ReactDom.render(
-    <Provider store={createStore(reducers)}
-        ><App />
+    <Provider store={createStore(reducers)} >
+        <App />
     </Provider>, 
     document.getElementById('root'));
